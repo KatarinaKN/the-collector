a92aa8074272b729961b0a01d4d92d90120465f6
KatarinaKN <katarinak.nielsen@gmail.com>
2026-09-25 17:44:13 +0200

    Initial commit

---
+++ src/main/java/app/config/HibernateEmfBuilder.java
@@ -0,0 +1,35 @@
+package app.config;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+
+import jakarta.persistence.EntityManagerFactory;
+import java.util.Properties;
+
+
+final class HibernateEmfBuilder {
+
+    private HibernateEmfBuilder() {}
+
+    static EntityManagerFactory build(Properties props) {
+        try {
+            Configuration configuration = new Configuration();
+            configuration.setProperties(props);
+
+            EntityRegistry.registerEntities(configuration);
+
+            ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+                    .applySettings(configuration.getProperties())
+                    .build();
+
+            SessionFactory sf = configuration.buildSessionFactory(serviceRegistry);
+            return sf.unwrap(EntityManagerFactory.class);
+
+        } catch (Throwable ex) {
+            System.err.println("Initial SessionFactory creation failed: " + ex);
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+}
